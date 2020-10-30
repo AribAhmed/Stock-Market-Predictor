86e751f2cc61ba170d4227adeb0c5ee9d2af58d0
Arib Ahmed <airarib@gmail.com>
2020-10-29 19:40:36 -0700

    basic stock predictor

--- stockmarket.py
+++ stockmarket.py
@@ -23,5 +23,21 @@ monthlyStock = data.DataReader('TSLA', 'yahoo', start , end)
 
 print(monthlyStock)
 
-print("Predicting value for the next date...")
+print("Predicting highest value for the next date...")
 days_after = (date.today()+timedelta(days=1)).isoformat()
+
+highColumn = monthlyStock["High"]
+lowColumn = monthlyStock["Low"]
+
+avgColumn = highColumn-lowColumn
+
+meanData = avgColumn.mean()
+
+lenColumn = len(highColumn)
+
+lastData = highColumn[lenColumn-1] + lowColumn[lenColumn-1]
+lastData = lastData/2
+
+newData = lastData + meanData
+
+print(newData)
